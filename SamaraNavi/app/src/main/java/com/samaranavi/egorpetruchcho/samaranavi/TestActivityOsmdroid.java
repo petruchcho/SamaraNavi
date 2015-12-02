7a847cb34b45619b0e4587f2c0d9b25221c56b43
Egor Ponomarev <egorponomarev.petruchcho@gmail.com>
2015-12-02 21:07:39 +0400

    Introduce TransformationUtils. Test setCenter method and showing my current location marker.

--- SamaraNavi/app/src/main/java/com/samaranavi/egorpetruchcho/samaranavi/TestActivityOsmdroid.java
+++ SamaraNavi/app/src/main/java/com/samaranavi/egorpetruchcho/samaranavi/TestActivityOsmdroid.java
@@ -5,15 +5,23 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.ViewGroup;
 import android.widget.LinearLayout;
 
+import com.samaranavi.egorpetruchcho.samaranavi.utils.TransformationUtils;
+
 import org.osmdroid.DefaultResourceProxyImpl;
+import org.osmdroid.api.IGeoPoint;
 import org.osmdroid.tileprovider.MapTileProviderBasic;
 import org.osmdroid.tileprovider.tilesource.ITileSource;
 import org.osmdroid.tileprovider.tilesource.WMSTileSource;
+import org.osmdroid.util.GeoPoint;
 import org.osmdroid.views.MapView;
+import org.osmdroid.views.overlay.mylocation.GpsMyLocationProvider;
+import org.osmdroid.views.overlay.mylocation.MyLocationNewOverlay;
 
 public class TestActivityOsmdroid extends AppCompatActivity {
 
     private static final String URL = "https://geoportal.samregion.ru/wms13?REQUEST=GetMap&LAYERS=E3,E2,E1&WIDTH=300&HEIGHT=300&FORMAT=image/png&TRANSPARENT=true&crs=EPSG:3857&srs=EPSG:3857&version=1.3.0&BBOX=";
+    private MapView mapView;
+    private MyLocationNewOverlay myLocationNewOverlay;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,21 +35,45 @@ public class TestActivityOsmdroid extends AppCompatActivity {
         final MapTileProviderBasic tileProvider = new MapTileProviderBasic(getApplicationContext());
 
         // create the WMS tile source
-        final ITileSource tileSource = new WMSTileSource("WMS", 1, 20, 300, ".png", new String[] {URL});
+        final ITileSource tileSource = new WMSTileSource("WMS", 1, 20, 300, ".png", new String[]{URL});
         tileProvider.setTileSource(tileSource);
 
         // create a new basic map view
-        MapView mapView = new MapView(this, new DefaultResourceProxyImpl(this), tileProvider);
+        mapView = new MapView(this, new DefaultResourceProxyImpl(this), tileProvider);
 
         // add the layout params to the view so the map fills the screen
         mapView.setLayoutParams(new ViewGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT));
 
-        mapView.getController().setZoom(3);
+        // Should be received from server in normal activity(no TestActivity)
+        IGeoPoint transformedGeoPoint = TransformationUtils.transformWebMercatorToWGS84(new GeoPoint(7023443, 5587902));
+        int currentZoom = 10;
+        mapView.getController().setZoom(currentZoom);
+        mapView.getController().setCenter(transformedGeoPoint);
 
         mapView.setBuiltInZoomControls(true);
         mapView.setMultiTouchControls(true);
 
+        // Add MyLocationMarker
+        GpsMyLocationProvider myLocationProvider = new GpsMyLocationProvider(this);
+        myLocationNewOverlay = new MyLocationNewOverlay(this, myLocationProvider, mapView);
+        mapView.getOverlays().add(myLocationNewOverlay);
+
         // add the mapview to display
         mapHolder.addView(mapView);
+
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        myLocationNewOverlay.enableMyLocation();
+        myLocationNewOverlay.enableFollowLocation();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        myLocationNewOverlay.disableMyLocation();
+        myLocationNewOverlay.disableFollowLocation();
     }
 }
---
+++ SamaraNavi/app/src/main/java/com/samaranavi/egorpetruchcho/samaranavi/utils/TransformationUtils.java
@@ -0,0 +1,61 @@
+package com.samaranavi.egorpetruchcho.samaranavi.utils;
+
+import org.osmdroid.api.IGeoPoint;
+import org.osmdroid.util.GeoPoint;
+
+public class TransformationUtils {
+    public static IGeoPoint transformWebMercatorToWGS84(IGeoPoint srcPoint) {
+        double srcPointX = srcPoint.getLatitudeE6();
+        double srcPointY = srcPoint.getLongitudeE6();
+
+        double num1 = 1.5707963267948966 - (2.0 * Math.atan(Math.exp((-1.0 * srcPointX) / 6378137.0)));
+        double num3 = srcPointY / 6378137.0;
+        double num4 = num3 * 57.295779513082323;
+        double num5 = Math.floor((num4 + 180.0) / 360.0);
+        double num6 = num4 - (num5 * 360.0);
+
+        return new GeoPoint(num1 * 57.295779513082323, num6);
+    }
+
+    public static WebMercatorGeoPoint transformWGS84ToWebMercator(IGeoPoint srcPoint) {
+        double srcPointX = srcPoint.getLatitude();
+        double srcPointY = srcPoint.getLongitude();
+
+        double num = srcPointX * 0.017453292519943295;
+        double x = 6378137.0 * num;
+        double a = srcPointY * 0.017453292519943295;
+
+        return new WebMercatorGeoPoint(new GeoPoint(3189068.5 * Math.log((1.0 + Math.sin(a)) / (1.0 - Math.sin(a))), x));
+    }
+
+    /**
+     * Simple decorator to know if we are using WebMercator
+     */
+    private static class WebMercatorGeoPoint implements IGeoPoint {
+        private final IGeoPoint geoPoint;
+
+        public WebMercatorGeoPoint(IGeoPoint geoPoint) {
+            this.geoPoint = geoPoint;
+        }
+
+        @Override
+        public int getLatitudeE6() {
+            return geoPoint.getLatitudeE6();
+        }
+
+        @Override
+        public int getLongitudeE6() {
+            return geoPoint.getLongitudeE6();
+        }
+
+        @Override
+        public double getLatitude() {
+            return geoPoint.getLatitude();
+        }
+
+        @Override
+        public double getLongitude() {
+            return geoPoint.getLongitude();
+        }
+    }
+}
